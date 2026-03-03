6d02d0777e349bd4ef27d168fe0b2415480002c4
Craig Rivers <craigrivers@mac.com>
2026-03-02 22:51:03 -0500

    Updated instructions for starting the app locally

--- demo/start_backend_locally.sh
+++ demo/start_backend_locally.sh
@@ -2,3 +2,6 @@
 mvnw -DskipTests install
 cp ladderbackend/target/demo-1.0.0-SNAPSHOT.war app.war
 mvnw pre-integration-test -Prun-full-stack
+#To start the frontend locally, uncomment the following lines and run:
+#cd ladderfrontend
+#ng serve
